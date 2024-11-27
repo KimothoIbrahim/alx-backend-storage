f0c04dcd370cf663d5b93d278cd611a0326c3319
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-11-27 17:42:49 +0300

    Order correctly

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Script
 
 -- Rank bands by number of fans
-SELECT origin, fans as nb_fans FROM (SELECT origin, SUM(fans) as fans FROM `metal_bands` GROUP BY origin) as band ORDER BY fans DESC;
+SELECT origin, fans as nb_fans FROM (SELECT origin, SUM(fans) as fans FROM `metal_bands` GROUP BY origin) as band ORDER BY fans DESC, origin;
